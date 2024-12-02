ceb6c457a9eb1d042a22c4a28241347fcd0784a3
taehoy <rethnoly@gmail.com>
2024-12-02 06:21:33 +0000

    file_page 구조체 선언

--- include/vm/file.h
+++ include/vm/file.h
@@ -7,6 +7,9 @@ struct page;
 enum vm_type;
 
 struct file_page {
+	struct file *file;		// 매핑된 파일 객체
+    off_t offset;			// 매핑된 파일에서의 시작 위치
+    size_t page_read_bytes;	// 페이지에 매핑된 파일에서 읽어올 바이트 수
 };
 
 void vm_file_init (void);
