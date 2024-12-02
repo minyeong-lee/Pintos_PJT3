66ecf3e3ff8017c744467398a995d0cc1f5234b9
taehoy <rethnoly@gmail.com>
2024-12-02 05:14:12 +0000

    do_mmap() 구현

--- vm/file.c
+++ vm/file.c
@@ -2,6 +2,10 @@
 
 #include "vm/vm.h"
 
+/** Project 3: Memory Mapped Files */
+#include "threads/vaddr.h"
+#include "userprog/process.h"
+
 static bool file_backed_swap_in (struct page *page, void *kva);
 static bool file_backed_swap_out (struct page *page);
 static void file_backed_destroy (struct page *page);
@@ -47,9 +51,51 @@ file_backed_destroy (struct page *page) {
 }
 
 /* Do the mmap */
+/**
+ * addr : 매핑을 시작할 가상 주소
+ * length : 매핑할 메모리 영역 크기 (바이트 단위)
+ * writable : 매핑된 메모리가 쓰기 가능한지 여부
+ * file : 매핑할 파일 객체
+ * offset : 파일에서 매핑을 시작할 오프셋
+ */
 void *
 do_mmap (void *addr, size_t length, int writable,
 		struct file *file, off_t offset) {
+	// 파일을 재오픈하여 독립적인 파일 객체 생성
+	struct file *mfile = file_reopen(file);
+
+	// 매핑 시작 주소 저장
+    void *ori_addr = addr;
+	// 매핑할 바이트 수와 남는 바이트 계산
+    size_t read_bytes = (length > file_length(file)) ? file_length(file) : length;
+    size_t zero_bytes = PGSIZE - read_bytes % PGSIZE;
+
+	// 페이지 단위로 메모리 매핑
+    while (read_bytes > 0 || zero_bytes > 0) {
+		// 현재 페이지에서 읽을 바이트와 0으로 채울 바이트 계산
+        size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
+        size_t page_zero_bytes = PGSIZE - page_read_bytes;
+
+		// 페이지 초기화 정보를 담을 container 생성
+        struct container *container = (struct container *)malloc(sizeof(struct container));
+        container->file = file;
+        container->offset = offset;
+        container->page_read_bytes = page_read_bytes;
+
+		// 페이지를 할당하고 초기화 정보와 함께 SPT 등록
+        if (!vm_alloc_page_with_initializer(VM_ANON, addr, writable, lazy_load_segment, container))
+            return false;
+
+		// 읽을 바이트와 남은 바이트 계산
+        read_bytes -= page_read_bytes;
+        zero_bytes -= page_zero_bytes;
+
+		// 다음 페이지로 이동
+        addr += PGSIZE;
+        offset += page_read_bytes;
+    }
+    
+    return ori_addr;	// 매핑 시작 주소 반환
 }
 
 /* Do the munmap */
